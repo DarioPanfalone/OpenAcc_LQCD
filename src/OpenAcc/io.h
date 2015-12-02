34d764e2224f32e1aff19103ced52e8c15c96649
Michele Mesiti <michele.mesiti@gmail.com>
2015-12-02 18:44:41 +0100

    Rationalized configuration reading/writing

---
+++ src/OpenAcc/io.h
@@ -0,0 +1,10 @@
+#ifndef IO_C_
+#define IO_C_
+
+#include "./struct_c_def.h"
+
+void print_su3_soa_ASCII(su3_soa * const conf, const char* nomefile,int conf_id_iter);
+void read_su3_soa_ASCII(su3_soa * conf, const char* nomefile,int * conf_id_iter );
+
+
+#endif 
