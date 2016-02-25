f83c07432cf51393310af45501b37cd607d41cd4
Michele Mesiti <michele.mesiti@gmail.com>
2016-02-25 01:01:57 +0100

    Added files which implement the fix.

---
+++ src/Meas/baryon_number_utilities.h
@@ -0,0 +1,24 @@
+#ifndef BARYON_NUMBER_UTILITIES_H_
+#define BARYON_NUMBER_UTILITIES_H_
+
+#include "../OpenAcc/struct_c_def.h"
+
+// if using GCC, there are some problems with __restrict.
+#ifdef __GNUC__
+ #define __restrict
+#endif
+
+void (*dM_dmu_eo[4])( __restrict su3_soa * const u,
+        vec3_soa * const out,
+        vec3_soa * const in,
+        double_soa * backfield);
+void (*dM_dmu_oe[4]) ( __restrict su3_soa * const u,
+        vec3_soa * const out,
+        vec3_soa * const in,
+        double_soa * backfield);
+    
+    
+    
+    
+
+#endif
