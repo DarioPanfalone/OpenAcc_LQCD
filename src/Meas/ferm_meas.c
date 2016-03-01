f12a028c13b62699d698d8636e297ad501daf2b4
Michele Mesiti <michele.mesiti@gmail.com>
2016-03-01 17:11:28 +0100

    Added TrM^-2 observable, to check

--- src/Meas/ferm_meas.c
+++ src/Meas/ferm_meas.c
@@ -85,6 +85,9 @@ void set_fermion_file_header(ferm_meas_params * fmpar, ferm_param * tferm_par){
             strcat(fmpar->fermionic_outfile_header,strtocat);
             sprintf(strtocat, "ReN_%-19sImN_%-19s",tferm_par[iflv].name,tferm_par[iflv].name);
             strcat(fmpar->fermionic_outfile_header,strtocat);
+            sprintf(strtocat, "ReTrM^-2_%-19sImTrM^-2_%-19s",tferm_par[iflv].name,tferm_par[iflv].name);
+            strcat(fmpar->fermionic_outfile_header,strtocat);
+
     }
     strcat(fmpar->fermionic_outfile_header,"\n");
 
@@ -97,7 +100,8 @@ void fermion_measures( su3_soa * tconf_acc,
         double res,
         int conf_id_iter  ){
     vec3_soa * rnd_e,* rnd_o;
-    vec3_soa * chi_e,* chi_o;//results of eo_inversion
+    vec3_soa * chi_e,* chi_o; //results of eo_inversion
+    vec3_soa *chi2_e,*chi2_o; //results of second eo_inversion, 
     vec3_soa * phi_e,* phi_o; // parking variables for eo_inverter
                               // also results of eo inversion multiplied by dM/dmu
     vec3_soa * trial_sol;
@@ -178,28 +182,39 @@ void fermion_measures( su3_soa * tconf_acc,
             generate_vec3_soa_gauss(trial_sol);
             d_complex chircond_size = 0.0 + 0.0*I;
             d_complex barnum_size = 0.0 + 0.0*I; // https://en.wikipedia.org/wiki/P._T._Barnum
+            d_complex trMinvSq_size = 0.0 + 0.0*I;
             double factor = tfermions_parameters[iflv].degeneracy*0.25/size;
+            double factor2 = factor*factor*size;
 
 #pragma acc data create(phi_e[0:1]) create(phi_o[0:1])\
             create(chi_e[0:1]) create(chi_o[0:1])   \
+            create(chi2_e[0:1]) create(chi2_o[0:1])   \
             copyin(rnd_e[0:1]) copyin(rnd_o[0:1]) copyin(trial_sol[0:1])
             {
                 // i fermioni ausiliari kloc_* sono quelli GLOBALI !!!
                 eo_inversion(conf_to_use,&tfermions_parameters[iflv],res,rnd_e,rnd_o,chi_e,chi_o,phi_e,phi_o,trial_sol,kloc_r,kloc_h,kloc_s,kloc_p);
-
                 chircond_size = scal_prod_global(rnd_e,chi_e)+
                     scal_prod_global(rnd_o,chi_o);
 
                 dM_dmu_eo[geom_par.tmap](conf_to_use,phi_e,chi_o,tfermions_parameters[iflv].phases);
                 dM_dmu_oe[geom_par.tmap](conf_to_use,phi_o,chi_e,tfermions_parameters[iflv].phases);
-
                 barnum_size = scal_prod_global(rnd_e,phi_e)+
                     scal_prod_global(rnd_o,phi_o);
+
+                eo_inversion(conf_to_use,&tfermions_parameters[iflv],res,chi2_e,chi2_o,chi_e,chi_o,phi_e,phi_o,trial_sol,kloc_r,kloc_h,kloc_s,kloc_p);
+
+                trMinvSq_size = scal_prod_global(chi2_e,rnd_e)+
+                    scal_prod_global(chi2_o,rnd_o); 
+
+
 
                 fprintf(foutfile,"%.16lf\t%.16lf\t",
                         creal(chircond_size*factor),cimag(chircond_size*factor));
                 fprintf(foutfile,"%.16lf\t%.16lf\t",
                         creal(barnum_size*factor),cimag(barnum_size*factor));
+                fprintf(foutfile,"%.16lf\t%.16lf\t",
+                        creal(trMinvSq_size*factor2),cimag(trMinvSq_size*factor2));
+
 
 
             }
