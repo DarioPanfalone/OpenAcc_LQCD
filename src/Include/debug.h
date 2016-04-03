4f95fc9d3ec8de20f141dab9129ded6e679eeac0
Michele Mesiti <michele.mesiti@gmail.com>
2016-04-03 22:19:35 +0200

    Added necessary debug.[ch] and montecarlo_parameters.[ch]

---
+++ src/Include/debug.h
@@ -0,0 +1,20 @@
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+typedef struct DEBUG_SETTINGS_T{
+
+    int use_ildg;
+    int input_vbl; // verbosity level
+    int save_diagnostics;
+    char diagnostics_filename[200];
+    int SaveAllAtEnd;
+    int do_reversibility_test;
+    int do_norandom_test;
+
+
+}debug_settings_t;
+
+extern debug_settings_t debug_settings; 
+
+
+#endif
