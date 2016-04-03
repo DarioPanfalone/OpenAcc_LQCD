c4207c9eef932b617e9faffea61e7fe32dd56780
Michele Mesiti <michele.mesiti@gmail.com>
2016-04-03 22:12:21 +0200

    Added necessary montecarlo_parameters.[ch]

---
+++ src/Include/montecarlo_parameters.c
@@ -0,0 +1,9 @@
+#ifndef _MONTECARLO_PARAMS_C_
+#define _MONTECARLO_PARAMS_C_
+
+#include "./montecarlo_parameters.h"
+
+
+mc_params_t mc_params;
+
+#endif
---
+++ src/Include/montecarlo_parameters.h
@@ -0,0 +1,20 @@
+#ifndef _MONTECARLO_PARAMS_H_
+#define _MONTECARLO_PARAMS_H_
+
+typedef struct MC_PARAM_T{
+    int ntraj                  ;
+    int MaxConfIdIter; 
+    double MaxRunTimeS ;
+    int therm_ntraj            ;
+    int storeconfinterval       ;
+    int saveconfinterval;
+    char RandGenStatusFilename[200];
+    char store_conf_name[200];
+    char save_conf_name[200];
+    int seed;
+    double eps_gen;
+} mc_params_t;
+
+extern mc_params_t mc_params;
+
+#endif
