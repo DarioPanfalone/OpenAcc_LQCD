11baccefe4f9db3366fe9ad947f89536d5ff5bcd
Michele Mesiti <michele.mesiti@swansea.ac.uk>
2019-10-10 16:49:19 +0100

    Added hook to GDB, can be activated with the GDBHOOK environment variable.

--- src/OpenAcc/main.c
+++ src/OpenAcc/main.c
@@ -54,7 +54,8 @@
 #include "sys/time.h"
 #endif
 
-
+#include <unistd.h>
+#include <mpi.h>
 #include "../Include/stringify.h"
 
 // double level macro, necessary to stringify
@@ -82,7 +83,24 @@ int main(int argc, char* argv[]){
     // READ input file.
 #ifdef MULTIDEVICE
     pre_init_multidev1D(&devinfo);
+    {
+        volatile int flag = 0;
+        if(0==devinfo.myrank && getenv("GDBHOOK")){
+            printf("MPI%02d: Thank you for using the GDB HOOK!\n",devinfo.myrank);
+            printf("         Waiting for user intervention.Please attach to process %d,\n",
+                    getpid());
+            printf("         [hint: gdb -p %d ] \n",  getpid());
+            printf("         and set the value of 'flag' to 1.\n");
+            printf("         You may have to use 'finish' a couple of times to go down the call stack.\n");
+            printf("         HAPPY HUNTING AND GOOD LUCK!!!!\n");
+            while(1 != flag)
+                sleep(1);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
 #endif
+
+
 
     if(0==devinfo.myrank){
         printf("****************************************************\n");
