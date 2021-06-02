66e45e8ccb7d0e09303aec508ff48540b0a90e3d
Lou-Ka <lucaparente95@gmail.com>
2021-06-02 17:05:39 +0200

    MOD_f

---
+++ src/OpenAcc/Mod_functions.c
@@ -0,0 +1,8 @@
+//
+//  Mod_functions.c
+//  
+//
+//  Created by Luca Parente on 02/06/21.
+//
+
+#include "Mod_functions.h"
---
+++ src/OpenAcc/Mod_functions.h
@@ -0,0 +1,13 @@
+//
+//  Mod_functions.h
+//  
+//
+//  Created by Luca Parente on 02/06/21.
+//
+
+#ifndef Mod_functions_h
+#define Mod_functions_h
+
+#include <stdio.h>
+
+#endif /* Mod_functions_h */
