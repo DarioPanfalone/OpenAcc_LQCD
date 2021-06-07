244b4381440ed5aa45a5698af24877f9f36384b7
Lou-Ka <lucaparente95@gmail.com>
2021-06-07 18:24:36 +0200

    parser mod X1

--- src/Include/setting_file_parser.h
+++ src/Include/setting_file_parser.h
@@ -6,6 +6,15 @@
 
 #define MAXCRLENGTH 300
 
+typedef struct rep_info_t{
+    
+    int replicas_total_number;
+    double cr_vet [MAXCRLENGTH];
+    
+    
+}rep_info;
+
+
 extern char input_file_str[MAXLINES*MAXLINELENGTH];
 
 int set_global_vars_and_fermions_from_input_file(const char* input_filename);
