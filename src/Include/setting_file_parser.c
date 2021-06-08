c334af97fb1f492b0ef7e28fc3b7c7750de4104e
Lou-Ka <lucaparente95@gmail.com>
2021-06-08 16:00:11 +0200

    mod parser X5

--- src/Include/setting_file_parser.c
+++ src/Include/setting_file_parser.c
@@ -914,7 +914,13 @@ int read_replicas_numbers(rep_info * re,char filelines[MAXLINES][MAXLINELENGTH],
            rp2[i2].comment=NULL;
            
        }
+    
+    while(startline<endline){
+    
+  int res = scan_group_NV(sizeof(rp2)/sizeof(par_info),rp2, filelines, startline+2, startline+3);
+        startline++
         
+    }
         
   
     /*
