a59ff0c8ef44aa41091f4c8d91e163aeafe7d6c3
Lou-Ka <lucaparente95@gmail.com>
2021-06-08 18:30:21 +0200

    mod parser X6

--- src/Include/setting_file_parser.c
+++ src/Include/setting_file_parser.c
@@ -904,9 +904,9 @@ int read_replicas_numbers(rep_info * re,char filelines[MAXLINES][MAXLINELENGTH],
   
     alloc_info.num_replicas=re->replicas_total_number;
     
-    char sec [10];
+    char sec [10]=10;
     char sec2[10]="noo";
-    sec=10;
+
     
     strcat(sec,sec2);
     
