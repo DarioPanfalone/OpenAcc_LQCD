8f1a9a14227ce16db49409b1caefec0d1e7ac686
Lou-Ka <lucaparente95@gmail.com>
2021-06-08 16:15:16 +0200

    mod parser X5

--- src/Include/setting_file_parser.c
+++ src/Include/setting_file_parser.c
@@ -911,7 +911,7 @@ int read_replicas_numbers(rep_info * re,char filelines[MAXLINES][MAXLINELENGTH],
            rp2[i2].par= &(re->cr_vet[i2]);
            
            rp2[i2].type=TYPE_DOUBLE;
-           rp2[i2].name="cr";
+           rp2[i2].name="cr_value";
            rp2[i2].default_value=NULL;
            rp2[i2].comment=NULL;
            
