91202bf1284e2acda36aeb3c145e3cbef8395389
Lou-Ka <lucaparente95@gmail.com>
2021-06-08 17:01:33 +0200

    mod parser X6

--- src/Include/setting_file_parser.c
+++ src/Include/setting_file_parser.c
@@ -246,7 +246,7 @@ int scan_group_NV(int npars,par_info* par_infos,char filelines[MAXLINES][MAXLINE
         // necessary step if a parameter name is contained 
         // in another parameter name.
 
-        reorder_par_infos(npars, par_infos);
+      /*  reorder_par_infos(npars, par_infos);*/
 
 
         int * rc = (int *) malloc(npars*sizeof(int));
@@ -900,35 +900,29 @@ int read_replicas_numbers(rep_info * re,char filelines[MAXLINES][MAXLINELENGTH],
    int res = scan_group_NV(sizeof(rp)/sizeof(par_info),rp, filelines, startline, startline+2);
   
     
-  
+       printf("%d\n",re->replicas_total_number);
     alloc_info.num_replicas=re->replicas_total_number;
   
-    /*
     par_info *rp2 ;
     rp2=malloc(alloc_info.num_replicas*sizeof(par_info));
     
 
        for(i2=0;i2<(alloc_info.num_replicas);i2++){
-          
            
            rp2[i2].par= &(re->cr_vet[i2]);
            
            rp2[i2].type=TYPE_DOUBLE;
-           rp2[i2].name=NULL;
+           rp2[i2].name="cr_value";
            rp2[i2].default_value=NULL;
            rp2[i2].comment=NULL;
            
        }
-     */
-    
     startline=startline+2;
     
     
     for(i2=0;i2<alloc_info.num_replicas;i2++){
     
-        re->cr_vet[i2]=filelines[startline];
-        
-  /*int res = scan_group_NV(alloc_info.num_replicas,rp2, filelines, startline, startline+1);*/
+  int res = scan_group_NV(alloc_info.num_replicas,rp2, filelines, startline, startline+1);
         startline++;
         
     }
