36ed2c7d2a07fab975a5c539d34612521c8eb64b
Lou-Ka <lucaparente95@gmail.com>
2021-06-23 17:16:35 +0200

    K_mu_mod/Plaq_mod

--- src/OpenAcc/Mod_functions.c
+++ src/OpenAcc/Mod_functions.c
@@ -88,6 +88,8 @@ int init_k(su3_soa * conf,double c_r,int def_axis,int * def_vet){
                                     
                                     
                                 counter=counter+1;
+                                
+                                 printf("(%d,%d,%d,%d):     k_mu[%d]=%f\n",i,j,z,t,snum_acc(i,j,z,t),conf[2*mu].K.d[snum_acc(i,j,z,t)]);
                      /* printf("(%d,%d,%d,%d):     k_mu[%d]=%f\n",2*i,j,z,t,snum_acc(2*i,j,z,t),conf[mu].K.d[snum_acc(2*i,j,z,t)]);*/
                                  }
                                 
@@ -130,7 +132,7 @@ int init_k(su3_soa * conf,double c_r,int def_axis,int * def_vet){
                                 
                                 
                                 counter=counter+1;
-                                /* printf("(%d,%d,%d,%d):     k_mu[%d]=%f\n",i,j,z,t,snum_acc(i,j,z,t),conf[mu].K.d[snum_acc(2*i,j,z,t)]);*/
+                                
                             }
                                 
                                 
