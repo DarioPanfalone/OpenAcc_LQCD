587b5a83e0940a0e065df0e5210ec3f382244698
Lou-Ka <lucaparente95@gmail.com>
2021-06-24 17:21:00 +0200

    Plaq_Mod_test1

--- src/OpenAcc/su3_measurements.c
+++ src/OpenAcc/su3_measurements.c
@@ -211,7 +211,7 @@ double  calc_plaquette_soloopenacc(
             // sommo i 6 risultati in tempo
             result  += calc_loc_plaquettes_nnptrick(tconf_acc,local_plaqs,tr_local_plaqs,mu,nu); //here ol the plaquettes of a specific plane's choice are computed.
             for(i_counter=0;i_counter<sizeh;i_counter++){
-                printf("ecco %f (%d) \n",tr_local_plaqs[0].c[5],i_counter;}
+                printf("ecco %f[%d] \n",tr_local_plaqs[0].c[5],i_counter);}
         }
     }
     
