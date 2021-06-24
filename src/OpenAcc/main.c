b017b2959114746ef587e445abd588995db0ab71
Lou-Ka <lucaparente95@gmail.com>
2021-06-24 17:04:19 +0200

    Plaq_Mod_test1

--- src/OpenAcc/main.c
+++ src/OpenAcc/main.c
@@ -453,7 +453,7 @@ int main(int argc, char* argv[]){
     /*
     int i_counter=0;
     
-    for (i_counter=0; i_counter<sizeh; i_counter++) {
+    for (i_counter; i_counter<sizeh; i_counter++) {
         printf("ecco (%d) : %f\n",i_counter,local_sums[0].c[i_counter]);
     }
     */
