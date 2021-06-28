326c374e8cdd5414a98cb1a7cb0212338a95ec1b
Lou-Ka <lucaparente95@gmail.com>
2021-06-28 19:05:30 +0200

    Plaq/Ret_Mod_test

--- src/OpenAcc/plaquettes.c
+++ src/OpenAcc/plaquettes.c
@@ -197,8 +197,8 @@ void calc_loc_staples_nnptrick_all(
         
             if(idxh==snum_acc(31,6,6,6) & parity==1 ){
                 /*printf("%d %d\n",iter,nu);*/
-                printf("%d %d\n",nu,iter);
-                 printf("CONTROLLO STAPLE[idxh] K_mu:%f[%d] (mu=%d, nu=%d)\n",K_mu,idxh,mu,iter);
+                printf("%d %d\n",mu,nu);
+                 printf("CONTROLLO STAPLE[idxh] K_mu:%f[%d] \n",K_mu,idxh);
             }
             
             
