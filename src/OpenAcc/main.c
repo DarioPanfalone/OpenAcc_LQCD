fd4ab8811edffc8b73ab90918e845c49ef1e615c
Lou-Ka <lucaparente95@gmail.com>
2021-07-02 17:41:39 +0200

    test2_conf_swap

--- src/OpenAcc/main.c
+++ src/OpenAcc/main.c
@@ -447,13 +447,13 @@ int main(int argc, char* argv[]){
     //Plaquette measures and polyakov loop measures.
     printf("PLAQUETTE START\n");
     
-    plq = calc_plaquette_soloopenacc(conf_hasenbusch[2],aux_conf_acc,local_sums);
+    plq = calc_plaquette_soloopenacc(conf_hasenbusch[0],aux_conf_acc,local_sums);
     printf("\tMPI%02d: Therm_iter %d Placchetta    = %.18lf \n",
             devinfo.myrank, conf_id_iter,plq/GL_SIZE/6.0/3.0);
     
     printf("PLAQUETTE END\n");
     
-    rect = calc_rettangolo_soloopenacc(conf_hasenbusch[2],aux_conf_acc,local_sums);
+    rect = calc_rettangolo_soloopenacc(conf_hasenbusch[0],aux_conf_acc,local_sums);
     printf("\tMPI%02d: Therm_iter %d Rettangolo    = %.18lf \n",
             devinfo.myrank, conf_id_iter,rect/GL_SIZE/6.0/3.0/2.0);
 
