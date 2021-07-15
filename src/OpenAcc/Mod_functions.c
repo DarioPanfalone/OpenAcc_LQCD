2bffd268dfec8971550abdab2df236f920e84de9
Lou-Ka <lucaparente95@gmail.com>
2021-07-15 13:05:44 +0200

    DELTA_S_MOD

--- src/OpenAcc/Mod_functions.c
+++ src/OpenAcc/Mod_functions.c
@@ -595,7 +595,7 @@ double calc_loc_plaquettes_nnptrick_SWAP(
 #pragma acc kernels present(u) present(w) present(loc_plaq) present(tr_local_plaqs)
 #pragma acc loop independent gang(STAPGANG3)
          
-                for(d3=D3-1_HALO; d3<D3-D3_HALO; d3++) {//what?
+                for(d3=-1+D3_HALO; d3<D3-D3_HALO; d3++) {//what?
 #pragma acc loop independent tile(STAPTILE0,STAPTILE1,STAPTILE2)
                     for(d2=-1; d2<D2; d2++) {
                         for(d1=-1; d1<D1; d1++) {
