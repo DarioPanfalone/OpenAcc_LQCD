30078008d8d0fac0d6bd191829b22f5782d6981c
Lou-Ka <lucaparente95@gmail.com>
2021-07-16 16:28:11 +0200

    DELTA_S_MOD

--- src/OpenAcc/Mod_functions.c
+++ src/OpenAcc/Mod_functions.c
@@ -106,8 +106,8 @@ int init_k(su3_soa * conf,double c_r,int def_axis,int * def_vet){
                            
                         
                             else{
-                                if (parity==0){conf[2*mu].K.d[snum_acc(i,j,z,t)]=1;}
-                                else{conf[2*mu+1].K.d[snum_acc(i,j,z,t)]=1; }
+                                if (parity==0){conf[2*mu].K.d[snum_acc(i,j,z,t)]=1; counter2=counter2+1;}
+                                else{conf[2*mu+1].K.d[snum_acc(i,j,z,t)]=1; counter2=counter2+1; }
                                 
                                 
                                 
@@ -148,8 +148,8 @@ int init_k(su3_soa * conf,double c_r,int def_axis,int * def_vet){
                                 
                                 
                             else{
-                                if (parity==0){conf[2*mu].K.d[snum_acc(i,j,z,t)]=1;}
-                                else{conf[2*mu+1].K.d[snum_acc(i,j,z,t)]=1;}
+                                if (parity==0){conf[2*mu].K.d[snum_acc(i,j,z,t)]=1; counter2=counter2+1;}
+                                else{conf[2*mu+1].K.d[snum_acc(i,j,z,t)]=1; counter2=counter2+1;}
                                 
                                 
                                 
@@ -190,9 +190,9 @@ int init_k(su3_soa * conf,double c_r,int def_axis,int * def_vet){
                                 
                                 
                             else{
-                                if (parity==0){conf[2*mu].K.d[snum_acc(i,j,z,t)]=1;}
+                                if (parity==0){conf[2*mu].K.d[snum_acc(i,j,z,t)]=1; counter2=counter2+1;}
                                 
-                                else{conf[2*mu+1].K.d[snum_acc(i,j,z,t)]=1;}
+                                else{conf[2*mu+1].K.d[snum_acc(i,j,z,t)]=1; counter2=counter2+1;}
                                 
                                 
                                 
