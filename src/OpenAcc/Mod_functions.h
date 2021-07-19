17817143b4ab723a38f87c3720411b7d025dd1eb
Lou-Ka <lucaparente95@gmail.com>
2021-07-19 19:33:05 +0200

    DELTA_S_MOD

--- src/OpenAcc/Mod_functions.h
+++ src/OpenAcc/Mod_functions.h
@@ -28,6 +28,7 @@ int replicas_swap_1(su3_soa * conf1,su3_soa * conf2,int def_axis,int * def_vet )
 int replicas_swap(su3_soa * conf1,su3_soa * conf2,int def_axis,int * def_vet );
 int label_print(su3_soa ** conf_hasen, int replicas_number,FILE *file,int step_number);
 
+int metro_SWAP(su3_soa ** conf_hasenbusch,int rep_indx1, int rep_indx2,int defect_axis,int * defect_coordinates);
 
 double calc_loc_plaquettes_rectangles_SWAP(
                                            __restrict const su3_soa * const u,//for an unknown reason the vet conf is called u. this is a vector odf su3_soa.
@@ -52,4 +53,3 @@ double  calc_plaquette_soloopenacc_SWAP( __restrict  su3_soa * const tconf_acc,
 
 #endif /* Mod_functions_h */
 
-int metro_SWAP(su3_soa ** conf_hasenbusch,int rep_indx1, int rep_indx2,int defect_axis,int * defect_coordinates);
