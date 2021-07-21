8a98a4932150bb5800cae795be834f73fc8e2534
Lou-Ka <lucaparente95@gmail.com>
2021-07-21 16:19:09 +0200

    MOD SWAP

--- src/OpenAcc/main.c
+++ src/OpenAcc/main.c
@@ -778,7 +778,7 @@ replicas_swap(conf_hasenbusch[0],conf_hasenbusch[2],rep->defect_boundary,rep->de
             if((conf_id_iter%5)==0){
             for(mu1=0;mu1<rep->replicas_total_number;mu1++){
                 mean_acceptance=(double)acceptance_vector[mu1]/all_swap_vector[mu1];
-                printf("replicas %d mean_acceptance\n ",mean_acceptance);
+                printf("replicas %d mean_acceptance\n ",mu1,mean_acceptance);
                 
             }
                 printf("\n");
