75899b9f460f97e3fed7970263ff1efc387ad73e
Lou-Ka <lucaparente95@gmail.com>
2021-07-21 17:25:43 +0200

    MOD SWAP

--- src/OpenAcc/Mod_functions.c
+++ src/OpenAcc/Mod_functions.c
@@ -2271,7 +2271,7 @@ void All_Conf_SWAP(su3_soa ** conf_hasenbusch,
             label_print(conf_hasenbusch, replicas_number,file_label,swap_num);
             accettata=metro_SWAP( conf_hasenbusch,loc_plaq,tr_local_plaqs, i_counter, i_counter+1,defect_axis,defect_coordinates);
             #pragma acc update device(conf_hasenbusch[0:replicas_number][0:8])
-            swap_num++;
+            swap_num=swap_num+1;
             label_print(conf_hasenbusch, replicas_number,file_label,swap_num);
             all_swap_vet[i_counter]++;
             all_swap_vet[i_counter+1]++;
@@ -2297,7 +2297,7 @@ void All_Conf_SWAP(su3_soa ** conf_hasenbusch,
         label_print(conf_hasenbusch, replicas_number,file_label,swap_num);
         accettata=metro_SWAP( conf_hasenbusch,loc_plaq,tr_local_plaqs, replicas_number-i_counter-1, replicas_number-i_counter-2,defect_axis,defect_coordinates);
 #pragma acc update device(conf_hasenbusch[0:replicas_number][0:8])
-            swap_num++;
+            swap_num=swap_num+1;
         label_print(conf_hasenbusch, replicas_number,file_label,swap_num);
             all_swap_vet[replicas_number-i_counter-1]++;
             all_swap_vet[replicas_number-i_counter-2]++;
