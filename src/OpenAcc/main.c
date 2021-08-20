661aa35231c69395814a6465a7137e442c3519e6
Lou-Ka <lucaparente95@gmail.com>
2021-08-20 18:47:57 +0200

    mod_acc

--- src/OpenAcc/main.c
+++ src/OpenAcc/main.c
@@ -586,11 +586,11 @@ replicas_swap(conf_hasenbusch[0],conf_hasenbusch[2],rep->defect_boundary,rep->de
     int *accettate_therm_old;
     int *accettate_metro_old;
     int id_iter_offset=conf_id_iter;
-    
-    printf("control 1\n" );
+
     accettate_therm=malloc(sizeof(int)*rep->replicas_total_number);
     accettate_metro=malloc(sizeof(int)*rep->replicas_total_number);
-        printf("control 2\n" );
+    iterations=malloc(sizeof(int)*rep->replicas_total_number);
+    
     
     //inizialization to 0
     int i;
