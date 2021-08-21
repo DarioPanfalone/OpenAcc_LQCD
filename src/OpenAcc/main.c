18bee0db5e89963ed4441804c92d9310aa177ae7
Lou-Ka <lucaparente95@gmail.com>
2021-08-21 03:17:28 +0200

    mod_acc

--- src/OpenAcc/main.c
+++ src/OpenAcc/main.c
@@ -810,7 +810,7 @@ replicas_swap(conf_hasenbusch[0],conf_hasenbusch[2],rep->defect_boundary,rep->de
 #pragma acc update self(conf_hasenbusch[0:rep->replicas_total_number][0:8]) //updating conf sul device
     
           //  if((conf_id_iter%5)==0){
-             fprintf(hmc_acc_file,"%d",conf_id_iter);
+             fprintf(hmc_acc_file,"%d   ",conf_id_iter);
             
             
             
@@ -822,12 +822,12 @@ replicas_swap(conf_hasenbusch[0],conf_hasenbusch[2],rep->defect_boundary,rep->de
                 mean_acceptance=(double)acceptance_vector[mu1]/all_swap_vector[mu1];
                 printf("replicas [%d]: proposed %d, accepted %d, mean_acceptance %f\n",mu1,all_swap_vector[mu1],acceptance_vector[mu1],mean_acceptance);
                 
-                fprintf(hmc_acc_file,"%d", accettate_therm[mu1]+accettate_metro[mu1]
+                fprintf(hmc_acc_file,"%d    ", accettate_therm[mu1]+accettate_metro[mu1]
                         -accettate_therm_old[mu1]-accettate_metro_old[mu1]);
                 printf("con %d \n",mu1);
             }
             printf("after  \n");
-           // fprintf(hmc_acc_file," \n");
+           fprintf(hmc_acc_file," \n");
             
           //  }
         //-----------------------------------------------//
