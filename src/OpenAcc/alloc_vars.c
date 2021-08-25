2ac6cafa4bc3b88eed5ccb0e5dec7b452319587c
Lou-Ka <lucaparente95@gmail.com>
2021-08-25 03:50:06 +0200

    mod

--- src/OpenAcc/alloc_vars.c
+++ src/OpenAcc/alloc_vars.c
@@ -364,7 +364,7 @@ void mem_free_core()
 
     }
     
-    #pragma acc exit data delete(conf_hasenbusch[0:alloc_info.num_replicas])
+#pragma acc exit data delete(conf_hasenbusch[0:alloc_info.num_replicas][0:8])
     
 
     FREECHECK(conf_hasenbusch);
