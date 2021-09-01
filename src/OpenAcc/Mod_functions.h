23dfbc50532d03153c87d5ad2b593344615f4185
Lou-Ka <lucaparente95@gmail.com>
2021-09-01 13:38:34 +0200

    debug

--- src/OpenAcc/Mod_functions.h
+++ src/OpenAcc/Mod_functions.h
@@ -18,7 +18,7 @@
 
 
 
-int init_k_test(su3_soa *conf_acc,int c_r);
+int init_k_test(su3_soa *conf_acc,double c_r);
 int n_replicas_reader(const char* input_filename);
 void counter_size_function(int d0,int d1,int d2,int d3);
 int init_k(su3_soa * conf,double c_r,int def_axis,int * def_vet);
