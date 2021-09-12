7e43946651f573c1d7209f9830fbe8403b3ffae2
Lou-Ka <lucaparente95@gmail.com>
2021-09-12 20:10:44 +0200

    trasl_debug

--- src/OpenAcc/main.c
+++ src/OpenAcc/main.c
@@ -882,24 +882,24 @@ replicas_swap(conf_hasenbusch[0],conf_hasenbusch[2],rep->defect_boundary,rep->de
                 int var1=snum_acc(1,1,1,1);
                 int var2=nnp_openacc[snum_acc(1,1,1,1)][0][0];
                 
-                   printf("del: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][0].r0.c0[var1],mu1,auxtris_conf_acc[0].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
-                      printf("del: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][0].r0.c0[snum_acc(1,1,1,1)],mu1,auxbis_conf_acc[0].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
+                   printf("del: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][0].r0.c0[var1],auxtris_conf_acc[0].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
+                      printf("del: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][0].r0.c0[snum_acc(1,1,1,1)],auxbis_conf_acc[0].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
                 
                 
              set_su3_soa_to_su3_soa(conf_hasenbusch[0],auxtris_conf_acc);
 
                 
-            printf("del: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][0].r0.c0[snum_acc(1,1,1,1)],mu1,auxtris_conf_acc[0].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
+            printf("del: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][0].r0.c0[snum_acc(1,1,1,1)],auxtris_conf_acc[0].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
                 
                 
                             set_su3_soa_to_su3_soa(conf_hasenbusch[0],auxbis_conf_acc);
                 
                 
-            printf("del: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][0].r0.c0[snum_acc(1,1,1,1)],mu1,auxbis_conf_acc[0].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
+            printf("del: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][0].r0.c0[snum_acc(1,1,1,1)],auxbis_conf_acc[0].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
                 
                 for(mu1=0;mu1<4;mu1++){
                  
-                    printf("before trasl: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][2*mu1].r0.c0[snum_acc(1,1,1,1)],mu1,conf_hasenbusch[0][2*mu1].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
+                    printf("before trasl: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][2*mu1].r0.c0[snum_acc(1,1,1,1)],conf_hasenbusch[0][2*mu1].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
                     
                    
                  }
@@ -911,7 +911,7 @@ replicas_swap(conf_hasenbusch[0],conf_hasenbusch[2],rep->defect_boundary,rep->de
                 
                 for(mu1=0;mu1<4;mu1++){
                     
-                    printf("after trasl: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][2*mu1].r0.c0[snum_acc(1,1,1,1)],mu1,conf_hasenbusch[0][2*mu1].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
+                    printf("after trasl: [mu1] %f %f || %f\n",mu1,conf_hasenbusch[0][2*mu1].r0.c0[snum_acc(1,1,1,1)],conf_hasenbusch[0][2*mu1].r0.c0[nnp_openacc[snum_acc(1,1,1,1)][mu1][0]]);
                     
                     
                 }
