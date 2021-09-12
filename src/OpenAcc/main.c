82c774dd69ae120df92c4dda5bf28c8159282992
Lou-Ka <lucaparente95@gmail.com>
2021-09-12 22:11:58 +0200

    trasl_debug

--- src/OpenAcc/main.c
+++ src/OpenAcc/main.c
@@ -891,12 +891,12 @@ replicas_swap(conf_hasenbusch[0],conf_hasenbusch[2],rep->defect_boundary,rep->de
                 set_su3_soa_to_zero(auxbis_conf_acc);
                 
                 
-            printf("del: [mu1] %f %f || %f\n",mu1,real(conf_hasenbusch[0][0].r0.c0[snum_acc(1,1,1,1)],auxtris_conf_acc[0].r0.c0[2]));
+            printf("del: [mu1] %f %f || %f\n",mu1,creal(conf_hasenbusch[0][0].r0.c0[snum_acc(1,1,1,1)],auxtris_conf_acc[0].r0.c0[2]));
                 
                 
                 
                 
-            printf("del: [mu1] %f %f || %f\n",mu1,real(conf_hasenbusch[0][0].r0.c0[snum_acc(1,1,1,1)],auxbis_conf_acc[0].r0.c0[2]));
+            printf("del: [mu1] %f %f || %f\n",mu1,creal(conf_hasenbusch[0][0].r0.c0[snum_acc(1,1,1,1)],auxbis_conf_acc[0].r0.c0[2]));
                 
                 
             /*
