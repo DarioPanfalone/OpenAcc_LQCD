5a9db8d83d20ab130991e2df31464711909eb584
Lou-Ka <lucaparente95@gmail.com>
2021-09-19 13:51:16 +0200

    multi_debug

--- src/OpenAcc/Mod_functions.c
+++ src/OpenAcc/Mod_functions.c
@@ -683,9 +683,9 @@ double calc_Delta_S_Wilson_SWAP(
                             if(d3==-1){idxh=snum_acc(d0,d1,d2,nd3-1);parity=(d0+d1+d2+(nd3-1))%2;}
                             */
                             
-                            if(d1==-1){ parity = (d0+d2+d3) % 2; idxh=nnm_openacc[snum_acc(d0,0,d2,d3)][1][parity];!parity;}
-                            if(d2==-1){parity = (d0+d1+d3) % 2; idxh=nnm_openacc[snum_acc(d0,d1,0,d3)][2][parity];!parity;}
-                            if(d3==-1+D3_HALO){parity = (d0+d1+d2+D3_HALO) % 2; idxh=nnm_openacc[snum_acc(d0,d1,d2,D3_HALO)][3][parity];!parity;}
+                            if(d1==-1){ parity = (d0+d2+d3) % 2; idxh=nnm_openacc[snum_acc(d0,0,d2,d3)][1][parity];parity=!parity;}
+                            if(d2==-1){parity = (d0+d1+d3) % 2; idxh=nnm_openacc[snum_acc(d0,d1,0,d3)][2][parity];parity=!parity;}
+                            if(d3==-1+D3_HALO){parity = (d0+d1+d2+D3_HALO) % 2; idxh=nnm_openacc[snum_acc(d0,d1,d2,D3_HALO)][3][parity];parity=!parity;}
                             
                        // idxh=nnm_openacc[idxh][nu][parity]; // the previous one. //MOD
                         
