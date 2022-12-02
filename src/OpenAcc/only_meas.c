bdff6df711b5cfea3d402f281be9f68faef9231c
DarioPanfalone <dpanfalo@login01.m100.cineca.it>
2022-12-02 14:56:04 +0100

    3 Starting point of the test with benchmarks

--- src/OpenAcc/only_meas.c
+++ src/OpenAcc/only_meas.c
@@ -64,7 +64,12 @@
 int conf_id_iter;
 int verbosity_lv=3;
 //
-#define STAMPA_DEBUG_SU3_SOA(var,dir,idx)\
+#define ALLOCCHECK(control_int,var)  if(control_int != 0 )              \
+		printf("MPI%02d: \tError in  allocation of %s . \n",devinfo.myrank, #var);\
+	else if(verbosity_lv > 2) printf("MPI%02d: \tAllocation of %s : OK , %p\n",\
+																	 devinfo.myrank, #var, var );\
+	
+#define STAMPA_DEBUG_SU3_SOA(var,dir,idx)																\
 	printf("%s[%d], idx %d:\n(%le,%le)    (%le,%le)    (%le,%le)\n(%le,%le)    (%le,%le)    (%le,%le)\n(%le,%le)    (%le,%le)    (%le,%le)\n\n", #var, dir, idx, \
 		creal(var[dir].r0.c0[idx]),cimag(var[dir].r0.c0[idx]),creal(var[dir].r0.c1[idx]),cimag(var[dir].r0.c1[idx]),creal(var[dir].r0.c2[idx]),cimag(var[dir].r0.c2[idx]), \
 		creal(var[dir].r1.c0[idx]),cimag(var[dir].r1.c0[idx]),creal(var[dir].r1.c1[idx]),cimag(var[dir].r1.c1[idx]),creal(var[dir].r1.c2[idx]),cimag(var[dir].r1.c2[idx]), \
@@ -79,6 +84,7 @@ int main(int argc, char **argv){
     su3_soa *  field_corr;
 		su3_soa *  field_corr_aux;
 		su3_soa * conf_au;
+		su3_soa * aux_staple;
 		single_su3 * closed_corr;
 		single_su3 * loc_plaq_aux;
 		double plq, rect;
@@ -91,11 +97,7 @@ int main(int argc, char **argv){
 //Variabili per lettura file conf
 		char confs[MAX][MAX], file[MAX],  nome[MAX];
 		FILE *fp;
-#define ALLOCCHECK(control_int,var)  if(control_int != 0 )							\
-    printf("MPI%02d: \tError in  allocation of %s . \n",devinfo.myrank, #var);\
-    else if(verbosity_lv > 2) printf("MPI%02d: \tAllocation of %s : OK , %p\n",\
-         devinfo.myrank, #var, var );\
-	
+		
 	 	allocation_check =  POSIX_MEMALIGN_WRAPPER((void **)&conf, ALIGN,8*sizeof(global_su3_soa));
 		ALLOCCHECK(allocation_check, conf);
 #pragma acc enter data create(conf[0:8])
@@ -105,6 +107,9 @@ int main(int argc, char **argv){
 		allocation_check =  POSIX_MEMALIGN_WRAPPER((void **)&conf_au, ALIGN, 8*sizeof(su3_soa));
 		ALLOCCHECK(allocation_check, conf_au);
 #pragma acc enter data create(conf_au[0:8])
+		allocation_check =  POSIX_MEMALIGN_WRAPPER((void **)&aux_staple, ALIGN, 8*sizeof(su3_soa));
+		ALLOCCHECK(allocation_check, aux_staple);
+#pragma acc enter data create(aux_staple[0:8])
 		allocation_check =  POSIX_MEMALIGN_WRAPPER((void **)&local_sum, ALIGN, 2*sizeof(dcomplex_soa));
 		ALLOCCHECK(allocation_check, local_sum) ;
 #pragma acc enter data create(local_sum[0:2])
@@ -165,20 +170,21 @@ int main(int argc, char **argv){
 		geom_par.gnz = GL_N2 ;
 		geom_par.gnt = GL_N3 ;
 
-	 	geom_par.xmap = 3 ;
-	 	geom_par.ymap = 2 ;
-		geom_par.zmap = 1 ;
-		geom_par.tmap = 0 ;
+	 	geom_par.xmap = 0 ;
+	 	geom_par.ymap = 1 ;
+		geom_par.zmap = 2 ;
+		geom_par.tmap = 3 ;
 
 		set_geom_glv(&geom_par);
 		
  	//	global_su3_soa * conf_rw = (global_su3_soa * ) malloc(8*sizeof(global_su3_soa));	
  		int conf_id;
 		//argv[0]="save_conf";
-		printf("Reading conf..\n");
+		//printf("Reading conf..\n");
 		//int r=read_su3_soa_ASCII(&conf_rw,"save_conf",&conf_id);
 		int dim=lettura_parole(argv[1], confs);
 		//strcpy(nome, confs[0]);
+
 		int  r = read_su3_soa_ildg_binary(conf,confs[0],&conf_id); 
 		
 	 	if(0==r){
@@ -189,22 +195,27 @@ int main(int argc, char **argv){
 		}
 		//printf("fatto\n");
 		//prova stampa link
-		int d0=1, d1=11, d2=1, d3=D3_HALO;
+	int d0=1, d1=11, d2=1, d3=D3_HALO;
 		int muh=1;
 		int idxh = snum_acc(d0, d1, d2, d3);
 		int parity = (d0 + d1 + d2 + d3) % 2;
 		int dir_link = 2*muh + parity;
-		
+		//cooling
+		int maxstep=10;
+		fp=fopen(argv[2], "w");
+	for(int coolstep=0; coolstep<maxstep; coolstep++){
+
+			cool_conf(u ,u, aux_staple);
 	  
 #pragma acc update self(u[0:8])
-		STAMPA_DEBUG_SU3_SOA(u,dir_link,idxh);
+			STAMPA_DEBUG_SU3_SOA(u,dir_link,idxh);
 
 			plq = calc_plaquette_soloopenacc(u, conf_au, local_sum);
 		//		rect = calc_rettangolo_soloopenacc(u, conf_au, local_sum);
 		//	printf("rettangolo     : %.18lf\n" ,rect);
 		printf("Plaquette     : %.18lf\n" ,plq/GL_SIZE/3.0/6.0);
 
-		fp=fopen(argv[2], "w");
+		//	fp=fopen(argv[2], "w");
 	  for(int ro=0; ro<4; ro++){ 
 			for(int mu=0 ; mu<3; mu++){ 
 							for(int nu=mu+1; nu<4; nu++){           
@@ -215,12 +226,13 @@ int main(int argc, char **argv){
 //STAMPA_DEBUG_SU3_SOA(field_corr,dir_link,idxh);
 #pragma acc data copyout(trace[0:nd0])					
 					 for(int L=1; L<=nd0/2; L++) { 
-						 fprintf(fp,"%d: %d\t%d\t%d\t(%.18lf,%.18lf)\n", L, ro, mu, nu, 0.5*creal(trace[L])/GL_SIZE, cimag(trace[L]));
+						 fprintf(fp," %d\t%d: %d\t%d\t%d\t%.18lf\n", coolstep, L, ro, mu, nu, 0.5*creal(trace[L])/GL_SIZE);
 						 //       fprintf(fp,"%d;%d;%d;%d;%lf\n", mu, nu, ro, L, traccia[L]); 
                                          } 
                              } 
 		                   }    
 					   }
+	}
 		fclose(fp);
 #pragma acc exit data delete(nnp_openacc)
 #pragma acc exit data delete(nnm_openacc)					 
